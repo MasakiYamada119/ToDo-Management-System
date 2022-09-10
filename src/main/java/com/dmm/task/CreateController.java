b3ecd57b18a05da287a91696729706ee3d267ee3
masakiyamada <109943822+MasakiYamada119@users.noreply.github.com>
2022-09-10 16:09:41 +0900

    質問用

--- src/main/java/com/dmm/task/CreateController.java
+++ src/main/java/com/dmm/task/CreateController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.dmm.task.data.entity.Tasks;
 import com.dmm.task.data.repository.TasksRepository;
@@ -23,24 +24,33 @@ public class CreateController {
 	private TasksRepository repo;
 		
 	@GetMapping("/main/create/{date}")
-	public String create(@Validated TaskForm taskForm, BindingResult bindingResult,
+	public String create(Model model) {
+			List<Tasks> list = repo.findAll(Sort.by(Sort.Direction.DESC, "id"));
+			model.addAttribute("create", list);
+			TaskForm taskForm = new TaskForm();
+			model.addAttribute("taskForm", taskForm);
+			return "/create";
+	}
+	@PostMapping("/main/create")
+	public String createpage(@Validated TaskForm taskForm, BindingResult bindingResult,
 			@AuthenticationPrincipal AccountUserDetails user, Model model) {
+		// バリデーションの結果、エラーがあるかどうかチェック
 		if (bindingResult.hasErrors()) {
+			// エラーがある場合は投稿登録画面を返す
 			List<Tasks> list = repo.findAll(Sort.by(Sort.Direction.DESC, "id"));
 			model.addAttribute("create", list);
 			model.addAttribute("taskForm", taskForm);
-			return "/create";
+			return "/main/create";
 		}
-			
-		Tasks task = new Tasks();
-		task.setName(user.getName());
-		task.setTitle(taskForm.getTitle());
-		task.setText(taskForm.getText());
-		task.setDate(LocalDateTime.now());
-		task.setDone(task.getDone());
+	    Tasks task = new Tasks();
+	    task.setName(user.getName());
+	    task.setTitle(taskForm.getTitle());
+	    task.setText(taskForm.getText());
+	    task.setDate(LocalDateTime.now());
+	    task.setDone(task.getDone());
 
-		repo.save(task);
+	    repo.save(task);
 
-		return "redirect:/main";
+	    return "redirect:/main";
 	}
 }
