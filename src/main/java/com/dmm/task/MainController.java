76cc35a1645f96b4f91d8c2a6e69155662f8db9c
masakiyamada <109943822+MasakiYamada119@users.noreply.github.com>
2022-09-12 00:34:17 +0900

    質問用

--- src/main/java/com/dmm/task/MainController.java
+++ src/main/java/com/dmm/task/MainController.java
@@ -25,14 +25,14 @@ public class MainController {
 	
 	@GetMapping("/main")
 	public String Calendar(@AuthenticationPrincipal AccountUserDetails user, Model model) {
-		LocalDate firstday = LocalDate.now().withDayOfMonth(1); //その月の1日のLocalDates
-	    DayOfWeek week = firstday.getDayOfWeek(); //曜日を表すDayOfWeek
-	    LocalDate lastmonth = firstday.minusDays(week.getValue()); //当月カレンダーの前月分
-	    LocalDate nextmonth = LocalDate.now().plusMonths(1L); //当月カレンダーの来月分(10月)
-	    LocalDate nextmonthOfFirstday = nextmonth.withDayOfMonth(1);
-	    LocalDate Lastmonth = LocalDate.now().minusMonths(1L); //当月カレンダーの来月分(8月)
-	    LocalDate LastmonthOfFirstday = Lastmonth.withDayOfMonth(1);
-	    LocalDate lastday = LocalDate.now().withDayOfMonth(30); //月末
+		LocalDate firstday = LocalDate.now().withDayOfMonth(1); //今月の1日のLocalDates
+	    DayOfWeek w = firstday.getDayOfWeek(); //曜日を表すDayOfWeek
+	    LocalDate lastMonth = firstday.minusDays(w.getValue()); //当月カレンダーの前月分
+	    LocalDate nextMonthAll = LocalDate.now().plusMonths(1L); //当月カレンダーの来月分(10月)
+	    LocalDate nextMonthOfFirstday = nextMonthAll.withDayOfMonth(1);
+	    LocalDate lastMonthAll = LocalDate.now().minusMonths(1L); //当月カレンダーの前月分(8月)
+	    LocalDate LastMonthOfFirstday = lastMonthAll.withDayOfMonth(1);
+	    final int lastDay = firstday.lengthOfMonth(); //月末
 	    
 	    MultiValueMap<LocalDate, Tasks> tasks = new LinkedMultiValueMap<LocalDate, Tasks>();
     	List<Tasks> list ;
@@ -42,87 +42,31 @@ public class MainController {
     	if (user.getAuthorities().stream().map(GrantedAuthority::getAuthority).anyMatch(a -> a.equals("ROLE_ADMIN"))) {
     	    list = repo.findAll(); //ROLE_ADMINならTasksの全情報を開示
     	} else {
-    	    list = repo.findByDateBetween(firstday.atTime(0,0), lastday.atTime(23,59), user.getName());
+    	    list = repo.findByDateBetween(firstday.atTime(0,0), nextMonthOfFirstday.atTime(23,59), user.getName());
     	}
     	for(Tasks t : list) {
     	    tasks.add(t.getDate().toLocalDate(), t);
     	}
          
-		List<List<LocalDate>> d = new ArrayList<>();
-        for (int i = 1; i <= 1; i++) {
-            List<LocalDate> d1 = new ArrayList<>(); //1週目のLocalDateを格納するList
-            for (int j = 1; j <= 1; j++) { 
-            	d1.add(lastmonth);
-            	d1.add(lastmonth.plusDays(1));
-            	d1.add(lastmonth.plusDays(2));
-            	d1.add(lastmonth.plusDays(3));
-            	d1.add(firstday);
-            	d1.add(firstday.plusDays(1));
-            	d1.add(firstday.plusDays(2));
+		List<List<LocalDate>> matrix = new ArrayList<>();
+            List<LocalDate> week = new ArrayList<>(); 
+            // 1週目（前月分を含む1週目）
+            for (int i = 1; i <= 7; i++) {
+            	week.add(lastMonth);
+            	lastMonth = lastMonth.plusDays(1);
             }
-            List<LocalDate> d2 = new ArrayList<>(); //2週目のLocalDateを格納するList
-            for (int j = 1; j <= 1; j++) { 
-            	d2.add(firstday.plusDays(3));
-            	d2.add(firstday.plusDays(4));
-            	d2.add(firstday.plusDays(5));
-            	d2.add(firstday.plusDays(6));
-            	d2.add(firstday.plusDays(7));
-            	d2.add(firstday.plusDays(8));
-            	d2.add(firstday.plusDays(9));
-            	if (week == DayOfWeek.SATURDAY) {
-            		System.out.println("\n");
-    			}
+            // 2週目（2週目から月末まで）
+            for(int i = 7; i <= lastDay; i++) {
+                if(w == DayOfWeek.SATURDAY){
+                    matrix.add(week);   // 月に週を追加
+                    week = new ArrayList<>();  // 新しい週のListを作成
+                }
+                firstday = firstday.plusDays(1);
             }
-            List<LocalDate> d3 = new ArrayList<>(); //3週目のLocalDateを格納するList
-            for (int j = 1; j <= 1; j++) { 
-            	d3.add(firstday.plusDays(10));
-            	d3.add(firstday.plusDays(11));
-            	d3.add(firstday.plusDays(12));
-            	d3.add(firstday.plusDays(13));
-            	d3.add(firstday.plusDays(14));
-            	d3.add(firstday.plusDays(15));
-            	d3.add(firstday.plusDays(16));
-            	if (week == DayOfWeek.SATURDAY) {
-            		System.out.println("\n");
-    			}
-            }
-            List<LocalDate> d4 = new ArrayList<>(); //4週目のLocalDateを格納するList
-            for (int j = 1; j <= 1; j++) { 
-            	d4.add(firstday.plusDays(17));
-            	d4.add(firstday.plusDays(18));
-            	d4.add(firstday.plusDays(19));
-            	d4.add(firstday.plusDays(20));
-            	d4.add(firstday.plusDays(21));
-            	d4.add(firstday.plusDays(22));
-            	d4.add(firstday.plusDays(23));
-            	if (week == DayOfWeek.SATURDAY) {
-            		System.out.println("\n");
-    			}
-            }
-            List<LocalDate> d5 = new ArrayList<>(); //5週目のLocalDateを格納するList
-            for (int j = 1; j <= 1; j++) { 
-            	d5.add(firstday.plusDays(24));
-            	d5.add(firstday.plusDays(25));
-            	d5.add(firstday.plusDays(26));
-            	d5.add(firstday.plusDays(27));
-            	d5.add(firstday.plusDays(28));
-            	d5.add(firstday.plusDays(29));
-            	d5.add(nextmonthOfFirstday);
-            	if (week == DayOfWeek.SATURDAY) {
-            		System.out.println("\n");
-    			}
-            }
-            d.add(d1);
-            d.add(d2);
-            d.add(d3);
-            d.add(d4);
-            d.add(d5);
-        }
     	model.addAttribute("tasks", tasks);
-		model.addAttribute("matrix", d);
-		model.addAttribute("prev", LastmonthOfFirstday);
-		model.addAttribute("next", nextmonthOfFirstday);
+		model.addAttribute("matrix", matrix);
+		model.addAttribute("prev", LastMonthOfFirstday);
+		model.addAttribute("next", nextMonthOfFirstday);
 		return "main";
 	}
-
 }
