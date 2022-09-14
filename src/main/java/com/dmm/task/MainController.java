5f88e35a54a1fcad823adcfc44c518f784b8de7b
masakiyamada <109943822+MasakiYamada119@users.noreply.github.com>
2022-09-14 23:18:48 +0900

    質問用

--- src/main/java/com/dmm/task/MainController.java
+++ src/main/java/com/dmm/task/MainController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -13,7 +14,6 @@ import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.dmm.task.data.entity.Tasks;
 import com.dmm.task.data.repository.TasksRepository;
@@ -25,22 +25,28 @@ public class MainController {
 	private TasksRepository repo;
 	
 	@GetMapping("/main")
-	public String Calendar(@AuthenticationPrincipal AccountUserDetails user, Model model) {
-		LocalDate day = LocalDate.now();
-		System.out.println(day);
-        LocalDate firstDay = LocalDate.of(day.getYear(), day.getMonthValue(), 1); //今月の1日のLocalDate;
-        System.out.println(firstDay);
-        DayOfWeek wf = firstDay.getDayOfWeek(); //1日の曜日を表すDayOfWeek
-        System.out.println(wf);
-        System.out.println(wf.getValue());
-        day = firstDay.minusDays(wf.getValue()); //今月カレンダーの前月分(8月28日)
-        System.out.println(day);
-        final int lastDay = firstDay.lengthOfMonth();
-        System.out.println(lastDay);
+	public String Calendar(@AuthenticationPrincipal AccountUserDetails user,
+			@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date, Model model) {
 		
 		List<List<LocalDate>> matrix = new ArrayList<>(); //週が入る行を作成するリスト
         List<LocalDate> week = new ArrayList<>(); //週のLocalDateを格納するリスト(自動的にmatrixに格納される)
-            
+		
+		LocalDate day;
+		if(date == null) {
+            // 引数のdateがnullだったら、今月と判断する ⇒ 今月のカレンダーを表示
+            day = LocalDate.now();
+            day = LocalDate.of(day.getYear(), day.getMonthValue(), 1);
+        }else {
+            // dateに値が入っていたら（①②の値が渡ってくる）、前月か来月の日付が入っている ⇒ 前月か来月のカレンダーを表示
+            day = date;
+        }
+		
+		model.addAttribute("prev", day.minusMonths(1));
+		model.addAttribute("next", day.plusMonths(1));
+		
+        DayOfWeek wf = day.getDayOfWeek(); //1日の曜日を表すDayOfWeek
+        day = day.minusDays(wf.getValue()); //今月カレンダーの前月分
+		
         // 1週目（前月分を含む1週目）
         for (int i = 1; i <= 7; i++) {
         	wf = day.getDayOfWeek();
@@ -71,48 +77,24 @@ public class MainController {
         }
         matrix.add(week);   // 月に週を追加
         
-        List<LocalDate> next = new ArrayList<>();
-        
-		for(int i = 1; i <= 12; i++) {
-			next.add(day);
-			day = day.plusMonths(1L);
-		}
-        
         MultiValueMap<LocalDate, Tasks> tasks = new LinkedMultiValueMap<LocalDate, Tasks>();
         List<Tasks> list ;
         
-	    LocalDate nextMonthAll = LocalDate.now().plusMonths(1L); //当月カレンダーの来月分(10月)
-	    LocalDate nextMonthOfFirstday = nextMonthAll.withDayOfMonth(1);
-	    LocalDate lastMonthAll = LocalDate.now().minusMonths(1L); //当月カレンダーの前月分(8月)
-	    LocalDate LastMonthOfFirstday = lastMonthAll.withDayOfMonth(1);
+        LocalDate firstDay = LocalDate.now().withDayOfMonth(1);
 
         //getAuthority(ユーザーに付与された権限を返すメソッド)を使いif文の条件式でstream処理(CollectionやListからデータを抽出する処理)を行い"ROLE_ADMIN"を判定する
         //.map(GrantedAuthority::getAuthority)でstream<String>をstream<collection>(getAuthorityの戻り値)に変換する
         if (user.getAuthorities().stream().map(GrantedAuthority::getAuthority).anyMatch(a -> a.equals("ROLE_ADMIN"))) {
-        	 list = repo.findByDateBetweenAdmin(firstDay.atTime(0,0), nextMonthOfFirstday.atTime(23,59)); //ROLE_ADMINならTasksの全情報を開示
+        	 list = repo.findByDateBetweenAdmin(firstDay.atTime(0,0), firstDay.plusMonths(1L).atTime(0,0)); //ROLE_ADMINならTasksの全情報を開示
         } else {
-        	 list = repo.findByDateBetweenUser(firstDay.atTime(0,0), nextMonthOfFirstday.atTime(23,59), user.getName());
+        	 list = repo.findByDateBetweenUser(firstDay.atTime(0,0), firstDay.plusMonths(1L).atTime(0,0), user.getName());
         }
         for(Tasks t : list) {
         	 tasks.add(t.getDate().toLocalDate(), t);
         }
+        
     	model.addAttribute("tasks", tasks);
 		model.addAttribute("matrix", matrix);
-		model.addAttribute("prev", LastMonthOfFirstday);
-		
 		return "main";
-	}
-	
-	@PostMapping("/main")
-	public String NextMonth(@AuthenticationPrincipal AccountUserDetails user,Model model) {
-		List<LocalDate> next = new ArrayList<>();
-		LocalDate day = LocalDate.now();
-		for(int i = 1; i <= 12; i++) {
-			next.add(day);
-			day = day.plusMonths(1L);
-		}
-		model.addAttribute("next", next);
-		return "redirect:/main";
-		
 	}
 }
